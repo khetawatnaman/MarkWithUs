92c9ecf42465f74d33e9c22ad9868e176307e9dc
Naman Khetawat <khetawat.naman1@gmail.com>
2018-09-13 14:17:30 +0530

    Adding all to git

---
+++ Mark/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contactus(request):
+    return render(request, 'contactus.html')
