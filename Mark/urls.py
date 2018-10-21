b182a88d604a4ce9eda4c6c14b00b9c5eef7a9e6
Naman Khetawat <khetawat.naman1@gmail.com>
2018-10-22 00:37:50 +0530

    Added links

--- Mark/urls.py
+++ Mark/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url, include
 from.import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about', views.about, name='about'),
-    path('contactus', views.contactus, name='contactus'),
+    url(r'^$', views.index, name= 'index'),
+    url(r'about', views.about, name='about'),
+    url(r'contactus', views.contactus, name='contactus'),
 
 ]
