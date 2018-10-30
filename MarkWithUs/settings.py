5e31a287a9707064e88c9cae5115ef51451c1caa
Naman Khetawat <khetawat.naman1@gmail.com>
2018-10-30 19:26:21 +0530

    updated settings

--- MarkWithUs/settings.py
+++ MarkWithUs/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -25,6 +26,7 @@ SECRET_KEY = '@x(lz^#@br+0u)z$6wtxuu(w5q*#cm3uh)vn(6&!%k@fhl*e!&'
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
 
 # Application definition
 
@@ -67,16 +69,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'MarkWithUs.wsgi.application'
 
+
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-# MySQLdb locally; when running in production on Google App Engine Standard
-# Environment, MySQLdb will be used.
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-# [END db_setup]
+}
 
 
 # Password validation
@@ -97,6 +99,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -110,12 +113,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-# #STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), '/var/www/static/',]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR, ]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), '/var/www/static/',]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
